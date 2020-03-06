e47263261d2e73429ac96e1c40daf9ab3b9484e7
jiaolyulu <54914804+jiaolyulu@users.noreply.github.com>
2020-03-06 13:09:50 -0500

    Update Rotate.cs

--- MR vehicle/MR Based Design and Experience Platform for Smart Vehicle cockpit/Assets/Rotate.cs
+++ MR vehicle/MR Based Design and Experience Platform for Smart Vehicle cockpit/Assets/Rotate.cs
@@ -7,12 +7,12 @@ public class Rotate : MonoBehaviour {
     private Quaternion targetRotation;
     private Quaternion thisRotation;
     public GameObject target;//the target object
-    private float speedMod = 10.0f;//a speed modifier
-    private Vector3 point;//the coord to the point where the camera looks at
+    private float speedMod = 10.0f;
+    private Vector3 point;
     void Start()
     {
-        point = target.transform.position;//get target's coords
-        transform.LookAt(point);//makes the camera look to it
+        point = target.transform.position;
+        transform.LookAt(point);
 
     }
 
